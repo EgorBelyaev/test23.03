cb79a026dcdd4ed22075488640aa84357c709146
Egor <belyaeff.egor2017@yandex.ru>
2020-10-02 17:19:40 +0300

    g
    fa

--- hello.rb
+++ hello.rb
@@ -1,7 +1,12 @@
-def hello
-    puts 'Hello World!'
+def hello number
+    i=0
+    while i<number
+        puts 'Hello World!'
+      i=i+1
+    end
 end
 
 if __FILE__ == $0
-    hello
+    hello 2
+    
 end    
